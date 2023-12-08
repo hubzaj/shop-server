39599e39fdd16eb9783b00ebea661b7fb205e251
Hubert Zajac <hubert.zajac@openx.com>
2023-12-08 11:18:41 +0100

    test data generator improvement

--- component-test/utils/test-data-generator.go
+++ component-test/utils/test-data-generator.go
@@ -1,15 +1,28 @@
 package utils
 
 import (
-	"crypto/rand"
+	randCrypto "crypto/rand"
 	"encoding/base64"
+	"golang.org/x/exp/rand"
 )
 
-func GenerateRandomString(length int) string {
+func GenerateRandomStringWithLen(length int) string {
 	b := make([]byte, length)
-	_, err := rand.Read(b)
+	_, err := randCrypto.Read(b)
 	if err != nil {
 		panic(err)
 	}
 	return base64.StdEncoding.EncodeToString(b)
+}
+
+func GenerateRandomString() string {
+	return GenerateRandomStringWithLen(10)
+}
+
+func GenerateRandFloatsInRange(min, max float64) float64 {
+	return min + rand.Float64()*(max-min)
+}
+
+func GenerateRandFloats() float64 {
+	return GenerateRandFloatsInRange(10, 100)
 }
